ccceb8266e5d38e1693cd8a2314b3c8efb2b404e
Renan Cunha <rfvcunha@hotmail.com>
2020-03-25 14:29:58 -0300

    Finalizando pagina de login

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 import './style.css';
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
-  return <h1>Hello!!</h1>;
+  return (
+    <div className="logonContainer">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input type="text" placeholder="Sua ID" />
+          <button type="submit" className="button">
+            Entrar
+          </button>
+
+          <Link to="/register">
+            <FiLogIn size={16} color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
 }
